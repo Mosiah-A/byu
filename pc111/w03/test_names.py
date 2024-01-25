6638de2c5c67bd6d323540d78201cdd00b205a04
Mosiah-A <mosiahassuncao@gmail.com>
2024-01-24 23:10:39 -0300

    initial commit

---
+++ pc111/w03/test_names.py
@@ -0,0 +1,47 @@
+from names import make_full_name, \
+    extract_given_name, extract_family_name
+import pytest
+
+
+def test_make_full_name():
+    """Verify that the make_full_name
+    function returns correct results.
+
+    Parameters: none
+    Return: nothing
+    """
+    assert make_full_name("Ava", "Smith-Jones") == "Smith-Jones;Ava"
+    assert make_full_name("James", "Madison") == "Madison;James"
+    assert make_full_name("J", "Ng") == "Ng;J"
+    assert make_full_name("", "") == ";"
+
+
+def test_extract_family_name():
+    """Verify that the extract_family_name
+    function returns correct results.
+
+    Parameters: none
+    Return: nothing
+    """
+    assert extract_family_name("Smith-Jones; Ava") == "Smith-Jones"
+    assert extract_family_name("Madison; James") == "Madison"
+    assert extract_family_name("Ng; J") == "Ng"
+    assert extract_family_name("; ") == ""
+
+
+def test_extract_given_name():
+    """Verify that the extract_given_name
+    function returns correct results.
+
+    Parameters: none
+    Return: nothing
+    """
+    assert extract_given_name("Smith-Jones; Ava") == "Ava"
+    assert extract_given_name("Madison; James") == "James"
+    assert extract_given_name("Ng; J") == "J"
+    assert extract_given_name("; ") == ""
+
+
+# Call the main function that is part of pytest so that
+# the test functions in this file will start executing.
+pytest.main(["-v", "--tb=line", "-rN", __file__])

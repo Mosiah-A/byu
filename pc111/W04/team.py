63a9e2a8b9ef92703556cda7cf2e875a90f6e62c
Mosiah-A <mosiahassuncao@gmail.com>
2024-01-30 14:17:43 -0300

    app

--- pc111/W04/team.py
+++ pc111/W04/team.py
@@ -1,8 +1,8 @@
 import random 
 
 def main():
-    number = [16.2, 75.1, 52.3]
-    print(number)
+    numbers = [16.2, 75.1, 52.3]
+    print(numbers)
     print()
     append_random_numbers(numbers)
     print(f"numbers {numbers}")
@@ -11,11 +11,11 @@ def main():
     print(f"numbers {numbers}")
     print()
 
-def append_random_numbers(numbers_list, quantity):
+def append_random_numbers(numbers_list, quantity=1):
     for _ in range(quantity):
         number = random.uniform(0, 100)
         rounded = round(number)
-        numbers_list.append(round)
+        numbers_list.append(rounded)
 
 if __name__ == '__main__':
     main()

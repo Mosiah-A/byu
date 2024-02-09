1bd162fdfc21e98e6ffdd5c642f5da02c22829db
Mosiah Andrade <54685972+Mosiah-A@users.noreply.github.com>
2024-02-09 15:29:05 -0300

    Update students.py

--- pc111/w05/students.py
+++ pc111/w05/students.py
@@ -1,46 +1,10 @@
-import csv
-I_NUM_INDEX = 0
-NAME_INDEX = 1
-
-def main():
-    search_ID = input('Please enter an I-Number (xxxxxxxx): ')
-    student = read_dictionary('students.csv', I_NUM_INDEX)
-    student = student[search_ID]
-    print(student[NAME_INDEX])
-
-def read_dictionary(filename, key_column_index):
-        """Read the contents of a CSV file into a compound
-        dictionary and return the dictionary.
-    
-        Parameters
-            filename: the name of the CSV file to read.
-            key_column_index: the index of the column
-                to use as the keys in the dictionary.
-        Return: a compound dictionary that contains
-            the contents of the CSV file.
-        """
-        dictionary = {}
-        
-
-        with open(filename, 'rt') as students_csv:
-                
-            reader = csv.reader(students_csv)
-
-            next(reader)
-
-            for row_list in reader:
-
-                if len(row_list) != 0:
-
-                    # From the current row, retrieve the data
-                    # from the column that contains the key.
-                    key = row_list[key_column_index]
-
-                    # Store the data from the current
-                    # row into the dictionary.
-                    dictionary[key] = row_list
-            return dictionary
-
-
-if __name__ == '__main__':
-        main()
+Traceback (most recent call last):
+  File "c:\Users\mosia\OneDrive\Área de Trabalho\byu-1\pc111\w05\receipt.py", line 25, in <module>
+    main()
+  File "c:\Users\mosia\OneDrive\Área de Trabalho\byu-1\pc111\w05\receipt.py", line 6, in main
+    dictionary = read_dictionary('products.csv')
+                 ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+  File "c:\Users\mosia\OneDrive\Área de Trabalho\byu-1\pc111\w05\receipt.py", line 12, in read_dictionary
+    with open(filename, 'rt') as csv_file:
+         ^^^^^^^^^^^^^^^^^^^^
+FileNotFoundError: [Errno 2] No such file or directory: 'products.csv'
